94bbbd505bee3f7c58d014dade25e7ebc0359468
hung <trnuongson2312@gmail.com>
2024-10-04 15:22:30 +0700

    fix buil errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import Navbar from '@/components/Navbar';
 import './globals.css'
 import type { Metadata } from "next";
-import {Inter, Space_Grotesk} from "next/font/google";
+import {Inter} from "next/font/google";
 
 
 const inter = Inter ({ subsets: ['latin']})
