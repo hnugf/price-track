a7e93f2a5956c11de2956d8fd4d2cd9f6a94115f
hung <trnuongson2312@gmail.com>
2024-10-09 15:14:22 +0700

    o

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,8 +1,8 @@
 import Product from "@/lib/models/product.model";
 import { connectToDB } from "@/lib/mongoose"
-//import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
-//import { scapeAmazonProduct } from "@/lib/scraper";
-//import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
+import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
+import { scapeAmazonProduct } from "@/lib/scraper";
+import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
 export const maxDuration = 60; //5minutes
@@ -18,57 +18,57 @@ export async function GET() {
 
         if(!products) throw new Error("No products found");
         //1. SCRAPE LATEST PRODUCT DETAILS & UPDATE DB
-        // const updatedProcuts = await Promise.all(
-        //     products.map(async (currentProduct) => {
-        //         const scapedProduct = await scapeAmazonProduct(currentProduct.url);
+        const updatedProcuts = await Promise.all(
+            products.map(async (currentProduct) => {
+                const scapedProduct = await scapeAmazonProduct(currentProduct.url);
 
-        //         if(!scapedProduct) throw new Error("No products found");
+                if(!scapedProduct) return;
 
 
-        //         const updatedPriceHistory = [
-        //             ...currentProduct.priceHistory,
-        //             { price: scapedProduct.currentPrice }
-        //         ]
+                const updatedPriceHistory = [
+                    ...currentProduct.priceHistory,
+                    { price: scapedProduct.currentPrice }
+                ]
     
-        //         const product = {
-        //             ...scapedProduct,
-        //             priceHistory: updatedPriceHistory,
-        //             lowestPrice: getLowestPrice(updatedPriceHistory),
-        //             highestPrice: getHighestPrice(updatedPriceHistory),
-        //             averagePrice: getAveragePrice(updatedPriceHistory),
-        //         }
+                const product = {
+                    ...scapedProduct,
+                    priceHistory: updatedPriceHistory,
+                    lowestPrice: getLowestPrice(updatedPriceHistory),
+                    highestPrice: getHighestPrice(updatedPriceHistory),
+                    averagePrice: getAveragePrice(updatedPriceHistory),
+                }
             
-        //     const updatedProcut = await Product.findOneAndUpdate( 
-        //         {  url: product.url },
-        //         product,
-        //     );
+            const updatedProcut = await Product.findOneAndUpdate( 
+                {  url: product.url },
+                product,
+            );
 
             
-        //         //2. CHECK EACH PRODCUT'S STATUS & SEND EMAIL ACCORDINGLY
-        //         const emailNotifType = getEmailNotifType(scapedProduct, currentProduct)
+                //2. CHECK EACH PRODCUT'S STATUS & SEND EMAIL ACCORDINGLY
+                const emailNotifType = getEmailNotifType(scapedProduct, currentProduct)
                  
 
-        //         if(emailNotifType && updatedProcut.users.lenght > 0) {
-        //             const productInfo = {
-        //                 title: updatedProcut.title,
-        //                 url: updatedProcut.url,
-        //                 image: product.image,
-        //             }
+                if(emailNotifType && updatedProcut.users.lenght > 0) {
+                    const productInfo = {
+                        title: updatedProcut.title,
+                        url: updatedProcut.url,
+                        image: product.image,
+                    }
 
-        //             const emailContent = await generateEmailBody(productInfo, emailNotifType);
+                    const emailContent = await generateEmailBody(productInfo, emailNotifType);
                     
-        //             const usersEmails = updatedProcut.users.map((user: any) => user.email)
+                    const usersEmails = updatedProcut.users.map((user: any) => user.email)
 
 
-        //             await sendEmail(emailContent, usersEmails);
-        //         }
+                    await sendEmail(emailContent, usersEmails);
+                }
 
-        //         return updatedProcut
-        //     })
-        // )
+                return updatedProcut
+            })
+        )
 
         return NextResponse.json({
-            message: 'OK', data: products.length
+            message: 'OK', data: updatedProcuts
         })
     } catch (error) {
         throw new Error(`Eroor in Get: ${error}`)
