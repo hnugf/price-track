61ab4fe2938d63f7b232cbd6e4b1d708b2a748ab
hung <trnuongson2312@gmail.com>
2024-10-09 15:38:56 +0700

    o

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -10,7 +10,7 @@ export const dynamic = 'force-dynamic'
 export const revalidate = 0;
 
 
-export async function GET(request: Request) {
+export async function GET() {
     try {
       connectToDB();
   
